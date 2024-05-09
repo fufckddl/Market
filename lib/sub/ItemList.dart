61e5f70263de298b2253310469fe08e137186836
Lee ChangRyoel <dlckdfuf141@naver.com>
2024-05-09 21:39:00 +0900

    Market Files

---
+++ lib/sub/ItemList.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Market Place',
+      home: ItemList(),
+    );
+  }
+}
+
+class ItemList extends StatefulWidget{
+  @override
+  State<ItemList> createState() => _ItemList();
+}
+
+class _ItemList extends State<ItemList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('물품목록'),
+      ),
+      body: Center(
+        child: Container(
+
+        ),
+      ),
+    );
+  }
+}
