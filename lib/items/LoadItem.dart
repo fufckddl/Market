6a19a4905b352f31d8d5bfcb16cce6e9637bcb77
Lee ChangRyoel <dlckdfuf141@naver.com>
2024-05-26 15:45:22 +0900

    - lib/data/ - lib/userinfo/
    제거

--- lib/items/LoadItem.dart
+++ lib/items/LoadItem.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -39,12 +43,52 @@ class ItemListPage extends StatelessWidget {
                 leading: Image.network(data['item_img_url']),
                 title: Text(data['item_name']),
                 subtitle: Text(data['item_info']),
-                trailing: Text(data['item_price'] + "원"),
+                trailing: Text('${data['item_price']}원'),
+                onTap: () {
+                  _showPurchaseDialog(context, data['item_name']);
+                },
               );
             },
           );
         },
       ),
+    );
+  }
+
+  void _showPurchaseDialog(BuildContext context, String itemName) {
+    final TextEditingController _quantityController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('구입할 개수를 입력하세요'),
+          content: TextField(
+            controller: _quantityController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: "개수"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('취소'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('확인'),
+              onPressed: () {
+                final int quantity = int.tryParse(_quantityController.text) ?? 0;
+                if (quantity > 0) {
+                  // Perform any action you want with the quantity
+                  print('Item: $itemName, Quantity: $quantity');
+                }
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
