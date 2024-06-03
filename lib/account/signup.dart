7b050401a60adf747bdc98d15b5d9290cc375f86
dfal0703 <84395375+dfal0703@users.noreply.github.com>
2024-06-03 22:07:58 +0900

    add login error

--- lib/account/signup.dart
+++ lib/account/signup.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '/account/login.dart';
 
@@ -17,14 +16,41 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _moneyController = TextEditingController();
 
   Future<void> _signUp() async {
-    final int? amount = int.tryParse(_moneyController.text);
+    final String id = _idController.text;
+    final String password = _passwordController.text;
+    final String name = _nameController.text;
+    final String email = _emailController.text;
+    final int? money = int.tryParse(_moneyController.text);
+
+    if (money == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: Money must be a valid number.'))
+      );
+      return;
+    }
+
     try {
+      // Firestore에서 동일한 ID가 있는지 확인
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('user_info')
+          .where('id', isEqualTo: id)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        // 동일한 ID가 이미 존재할 경우 예외 처리
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Error: ID already exists.'))
+        );
+        return;
+      }
+
+      // 동일한 ID가 없을 경우 회원가입 진행
       await FirebaseFirestore.instance.collection('user_info').add({
-        'id': _idController.text,
-        'password': _passwordController.text,
-        'name': _nameController.text,
-        'email': _emailController.text,
-        'money': _moneyController.text,
+        'id': id,
+        'password': password,
+        'name': name,
+        'email': email,
+        'money': money,
       });
 
       // 회원가입 후 로그인 페이지로 이동
@@ -67,7 +93,8 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
             TextField(
               controller: _moneyController,
-              decoration: InputDecoration(labelText: 'money'),
+              decoration: InputDecoration(labelText: 'Money'),
+              keyboardType: TextInputType.number, // 숫자 입력만 가능하게 설정
             ),
             ElevatedButton(
               onPressed: _signUp,
